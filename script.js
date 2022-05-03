061eb538af3758f62aac5c1fd6266b77534898f7
jurgengalicia <jurgen.galicia@gmail.com>
2022-05-02 22:24:54 -0700

    button syntax fix and clear all

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function renderTodo(todo) {
     <label for "${todo.id}" class="tick js-tick"></label>
     <span>${todo.x}</span>
     <button class="delete-todo js-delete-todo">
-        <button class="delete=todo js-delete-todo">
+        <button class="delete-todo js-delete-todo">
             <svg fill="var(--svgcolor)" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
             </svg>
